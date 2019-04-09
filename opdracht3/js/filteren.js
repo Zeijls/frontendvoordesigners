30846dd52a1387e27f1cf7dc3ca65ce62daf8ef0
Simone van Zeijl <simonev.zeijl@hotmail.nl>
2019-04-09 23:58:09 +0200

    Filters toegevoegd
    Er zijn verschililende filters toegepast aan de website. Hiervoor heb ik een nieuw javascript bestand aangemaakt: filteren.js. Er is nog niks aan de css aangepast

---
+++ opdracht3/js/filteren.js
@@ -0,0 +1,46 @@
+var body = document.querySelector('body');
+var alleGenresKnop = document.querySelector('.genres');
+var actionKnop = document.querySelector('.action');
+var adventureKnop = document.querySelector('.adventure');
+var crimeKnop = document.querySelector('.crime');
+var dramaKnop = document.querySelector('.drama');
+var horrorKnop = document.querySelector('.horror');
+var thrillerKnop = document.querySelector('.thriller');
+
+alleGenresKnop.addEventListener('click', function(){
+    body.classList = '';
+});
+
+
+actionKnop.addEventListener('click', function(){
+    body.classList = '';
+  body.classList.toggle('action');
+});
+
+adventureKnop.addEventListener('click', function(){
+    body.classList = '';
+  body.classList.toggle('adventure');
+});
+
+crimeKnop.addEventListener('click', function(){
+    body.classList = '';
+  body.classList.toggle('crime');
+});
+
+dramaKnop.addEventListener('click', function(){
+    body.classList = '';
+  body.classList.toggle('drama');
+});
+
+horrorKnop.addEventListener('click', function(){
+    body.classList = '';
+  body.classList.toggle('horror');
+});
+
+thrillerKnop.addEventListener('click', function(){
+    body.classList = '';
+  body.classList.toggle('thriller');
+});
+
+
+
